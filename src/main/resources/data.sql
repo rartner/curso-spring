0a263a491d9a814751160b80462c78158f7eb9a3
rartner <ramon.artner@gmail.com>
2018-07-30 21:53:47 -0300

    add command to delete db registers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
+DELETE FROM cidade;
 INSERT INTO cidade(nome) VALUES ('Joinville');
 INSERT INTO cidade(nome) VALUES ('Jaraguá do Sul');
 INSERT INTO cidade(nome) VALUES ('Blumenau');
 INSERT INTO cidade(nome) VALUES ('Canoinhas');
-
-DELETE FROM cidade WHERE id > 4;
